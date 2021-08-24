eae81812912868ff6bb6d87db83563de427e286c
nero-15 <nero15.dev@gmail.com>
2021-08-24 22:16:53 +0900

    hello rust

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\tak\devel\rust-learning-app\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\Users\tak\devel\rust-learning-app\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
