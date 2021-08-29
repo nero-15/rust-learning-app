5aca6d8f436f5a4c0125668eff65c96a1a2937b9
nero-15 <nero15.dev@gmail.com>
2021-08-29 11:16:36 +0900

    Shadowing

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
 
     const MAX_POINTS: u32 = 100_000;
     println!("{}", MAX_POINTS);
+
+    let x = 5;
+
+    println!("The value of x is: {}", x);
+
+    let x = x + 1;
+
+    println!("The value of x is: {}", x);
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
 }
