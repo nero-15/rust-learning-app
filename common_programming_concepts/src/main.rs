47bf7ff159cc1b81a9ffc00efb902d877073230e
nero-15 <nero15.dev@gmail.com>
2021-08-30 21:29:22 +0900

    string

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -60,4 +60,12 @@ fn main() {
 
     let f: bool = false; // with explicit type annotation
     println!("The value of f is: {}", f);
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("The value of c is: {}", c);
+    println!("The value of z is: {}", z);
+    println!("The value of heart_eyed_cat is: {}", heart_eyed_cat);
+
 }
