f8d3683c1430e2ac791ccc5ddf7680a6ba5feba3
nero-15 <nero15.dev@gmail.com>
2021-08-30 21:52:07 +0900

    fix

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
     //variables_and_mutability()
     //data_types()
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
 
 // https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
