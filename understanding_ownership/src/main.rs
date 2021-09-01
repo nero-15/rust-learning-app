8ff2e4f138eb8021bec75bdd1f1fd32462488bdd
nero-15 <nero15.dev@gmail.com>
2021-09-01 21:47:10 +0900

    fix

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{}", s); // This will print `hello, world!`
