04bb1060512c65130fd57d6d1fd8af7e0493b509
nero-15 <nero15.dev@gmail.com>
2021-09-02 21:20:02 +0900

    mv

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -3,4 +3,13 @@ fn main() {
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{}", s); // This will print `hello, world!`
+
+    let x = 5;
+    let y = x;
+
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{}, world!", s1);
 }
