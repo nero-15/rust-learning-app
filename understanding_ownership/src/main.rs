bf27b173292abd21e9a90c6c665908f467cea054
nero-15 <nero15.dev@gmail.com>
2021-09-02 21:31:52 +0900

    retun

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -10,10 +10,10 @@ fn main() {
     //
     // println!("{}, world!", s1);//error
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-
-    println!("s1 = {}, s2 = {}", s1, s2);
+    // let s1 = String::from("hello");
+    // let s2 = s1.clone();
+    //
+    // println!("s1 = {}, s2 = {}", s1, s2);
 
     let x = 5;
     let y = x;
@@ -30,6 +30,33 @@ fn main() {
     makes_copy(x);                  // x would move into the function,
                                     // but i32 is Copy, so it's okay to still
                                     // use x afterward
+
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+}
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
 }
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
