9305c5dc183812fc7d1b8cb2b7fde69b229a1306
nero-15 <nero15.dev@gmail.com>
2021-09-03 20:53:56 +0900

    fix

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -49,6 +49,14 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    // let s = String::from("hello");
+    //
+    // change(&s);//error
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", world");
 }
 
 fn calculate_length(s: &String) -> usize {
