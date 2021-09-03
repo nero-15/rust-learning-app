d26273a7415f08618de3d131528db04f8fe2deef
nero-15 <nero15.dev@gmail.com>
2021-09-03 21:00:27 +0900

    fix

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -53,6 +53,12 @@ fn main() {
     let mut s = String::from("hello");
 
     change(&mut s);
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
 fn change(some_string: &mut String) {
