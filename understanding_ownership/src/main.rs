70cd53d520b48fdbfbf6a4f400138f92dd2802e1
nero-15 <nero15.dev@gmail.com>
2021-09-03 20:48:28 +0900

    reference

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -42,9 +42,17 @@ fn main() {
 
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    // let (s2, len) = calculate_length(s1);
+    //
+    // println!("The length of '{}' is {}.", s2, len);
 
-    println!("The length of '{}' is {}.", s2, len);
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
 
 fn calculate_length(s: String) -> (String, usize) {
