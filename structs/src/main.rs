3aa2863987fb352030ba83d7939d1d5a814a3099
nero-15 <nero15.dev@gmail.com>
2021-09-04 22:31:19 +0900

    tuple struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -40,4 +40,10 @@ fn main() {
         ..user1
     };
     println!("{:?}", user3);
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
