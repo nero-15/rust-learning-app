850bbaa94cb6ecddaaa391d5570e4fb58ef47471
nero-15 <nero15.dev@gmail.com>
2021-09-04 21:57:53 +0900

    build_user

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,4 +17,20 @@ fn main() {
 
     user1.email = String::from("anotheremail@example.com");
     println!("{:?}", user1);
+
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email,
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+
+    let user2 = build_user(
+        String::from("tokatoka@example.com"),
+        String::from("tokatoka")
+    );
+    println!("{:?}", user2);
 }
