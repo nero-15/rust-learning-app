2d48fdcc7a4c3cb274edff64b078729569de149b
nero-15 <nero15.dev@gmail.com>
2021-09-04 21:49:39 +0900

    struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("{:?}", user1);
 }
