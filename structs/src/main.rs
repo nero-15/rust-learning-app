40b2ef8981f5917f9f7c118db69feb4825d2d97d
nero-15 <nero15.dev@gmail.com>
2021-09-05 15:02:28 +0900

    refactaring

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,17 +4,16 @@ fn main() {
 }
 
 fn example_structs(){
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
 
 fn creating_instances_from_other_instances_with_struct_update_syntax(){
