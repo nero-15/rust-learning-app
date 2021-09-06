b318bd809a73710c78e4bcbf41af4ba0d51c0fa2
nero-15 <nero15.dev@gmail.com>
2021-09-06 21:15:23 +0900

    Associated Functions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,9 +14,16 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
-    
+
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -46,6 +53,9 @@ fn method_syntax(){
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("{:?}", sq);
 }
 
 fn example_structs(){
