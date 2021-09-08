347b229585757960fc6a7474ebe741a274eef0db
nero-15 <nero15.dev@gmail.com>
2021-09-08 21:26:41 +0900

    enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:?}", four);
+    println!("{:?}", six);
 }
