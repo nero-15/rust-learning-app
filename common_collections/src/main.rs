8b6eff58fceb55f65e2f8156e86b31e5c04480d0
nero-15 <nero15.dev@gmail.com>
2021-09-13 21:44:16 +0900

    read

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     // let v: Vec<i32> = Vec::new();
-    let mut v = Vec::new();
+    // let mut v = Vec::new();
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
-    println!("{:?}", v)
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
+    // println!("{:?}", v)
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
 }
