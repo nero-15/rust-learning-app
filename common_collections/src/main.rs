3d7ab42618f7929b613ad7ff019583eeba7b58f9
nero-15 <nero15.dev@gmail.com>
2021-09-13 21:49:59 +0900

    for

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -8,16 +8,21 @@ fn main() {
     // v.push(8);
     // println!("{:?}", v)
 
-    let v = vec![1, 2, 3, 4, 5];
+    // let v = vec![1, 2, 3, 4, 5];
 
-    let third: &i32 = &v[2];
-    println!("The third element is {}", third);
+    // let third: &i32 = &v[2];
+    // println!("The third element is {}", third);
 
-    match v.get(2) {
-        Some(third) => println!("The third element is {}", third),
-        None => println!("There is no third element."),
-    }
+    // match v.get(2) {
+    //     Some(third) => println!("The third element is {}", third),
+    //     None => println!("There is no third element."),
+    // }
 
     // let does_not_exist = &v[100];
     // let does_not_exist = v.get(100);
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
 }
