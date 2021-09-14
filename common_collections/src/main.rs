52317ddc4ce0f7e93da4abd90f48a73618993436
nero-15 <nero15.dev@gmail.com>
2021-09-14 21:32:54 +0900

    hello

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -11,7 +11,30 @@ fn strings(){
 
     // the method also works on a literal directly:
     let s = "initial contents".to_string();
-    println!("{}", s)
+    println!("{}", s);
+
+    let hello = String::from("السلام عليكم");
+    println!("{}", hello);
+    let hello = String::from("Dobrý den");
+    println!("{}", hello);
+    let hello = String::from("Hello");
+    println!("{}", hello);
+    let hello = String::from("שָׁלוֹם");
+    println!("{}", hello);
+    let hello = String::from("नमस्ते");
+    println!("{}", hello);
+    let hello = String::from("こんにちは");
+    println!("{}", hello);
+    let hello = String::from("안녕하세요");
+    println!("{}", hello);
+    let hello = String::from("你好");
+    println!("{}", hello);
+    let hello = String::from("Olá");
+    println!("{}", hello);
+    let hello = String::from("Здравствуйте");
+    println!("{}", hello);
+    let hello = String::from("Hola");
+    println!("{}", hello);
 
 }
 
