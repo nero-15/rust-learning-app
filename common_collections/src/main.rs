0d0ced2b5c36d4285a437c2fc89de236b6d592f9
nero-15 <nero15.dev@gmail.com>
2021-09-14 21:35:10 +0900

    fix

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -40,6 +40,12 @@ fn strings(){
     s.push_str("bar");
     println!("{}", s);
 
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {}", s1);
+    println!("s2 is {}", s2);
+
 }
 
 fn vectors() {
