e326d9a43394bbd04002e0d3f7a3b71159a5d13c
nero-15 <nero15.dev@gmail.com>
2021-09-14 21:38:21 +0900

    format

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -46,6 +46,13 @@ fn strings(){
     println!("s1 is {}", s1);
     println!("s2 is {}", s2);
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s is {}", s);
+
 }
 
 fn vectors() {
