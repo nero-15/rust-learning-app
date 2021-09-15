6d94bde320d6767a5b8c35f90949887830a70675
nero-15 <nero15.dev@gmail.com>
2021-09-15 22:10:03 +0900

    fix

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -48,6 +48,17 @@ fn hash_maps(){
     scores.entry(String::from("Blue")).or_insert(50);
 
     println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
 
 fn strings(){
