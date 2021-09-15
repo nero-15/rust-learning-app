8401dcb04bf04bb2df7317d02b371cd0fcf5ac61
nero-15 <nero15.dev@gmail.com>
2021-09-15 21:53:38 +0900

    hashmap

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,6 +1,18 @@
 fn main(){
     //vectors()
-    strings();
+    //strings();
+    hash_maps() 
+}
+
+fn hash_maps(){
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:?}", scores);
 }
 
 fn strings(){
