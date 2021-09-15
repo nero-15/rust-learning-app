de4903daeffc2586feb31f80b14d495d949d9fa5
nero-15 <nero15.dev@gmail.com>
2021-09-15 22:04:03 +0900

    access

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -28,6 +28,15 @@ fn hash_maps(){
     let mut map = HashMap::new();
     map.insert(field_name, field_value);
     println!("{:?}", map);
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("{:?}", score);
 }
 
 fn strings(){
