b71634a7f6333ea60e7d0019d6522980ebf02ef9
nero-15 <nero15.dev@gmail.com>
2021-09-17 21:44:35 +0900

    unwrap

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -20,15 +20,16 @@ fn recoverable_errors_with_result(){
     //         }
     //     },
     // };
-    let f = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
+    // let f = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error);
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // });
+    let f = File::open("hello.txt").unwrap();
 }
 
 fn unrecoverable_errors_with_panic(){
