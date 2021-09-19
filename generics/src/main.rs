51fd8f387d7648fcf0a5bf2d129b494e01244536
nero-15 <nero15.dev@gmail.com>
2021-09-19 21:36:30 +0900

    struct

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,4 @@
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -10,26 +10,22 @@ fn largest_i32(list: &[i32]) -> i32 {
     largest
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    // let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    // let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
