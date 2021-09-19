04a735815484b862c437f755c1126625c5b0ea04
nero-15 <nero15.dev@gmail.com>
2021-09-19 21:38:29 +0900

    fix

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,18 +1,19 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
-}
+//     largest
+// }
 
-struct Point<T> {
+#[derive(Debug)]
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
@@ -26,6 +27,11 @@ fn main() {
     // let result = largest(&char_list);
     // println!("The largest char is {}", result);
 
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    println!("{:?}", both_integer);
+    println!("{:?}", both_float);
+    println!("{:?}", integer_and_float);
+
 }
