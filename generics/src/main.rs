e9e9f81aaff24b2948de6b51150f00e96bfb6cd7
nero-15 <nero15.dev@gmail.com>
2021-09-22 21:30:50 +0900

    ImportantExcerpt

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -43,21 +43,44 @@ fn main() {
     // println!("{:?}", both_float);
     // println!("{:?}", integer_and_float);
 
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    // let p1 = Point { x: 5, y: 10.4 };
+    // let p2 = Point { x: "Hello", y: 'c' };
 
-    let p3 = p1.mixup(p2);
+    // let p3 = p1.mixup(p2);
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    // println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 
-    let number_list = vec![34, 50, 25, 100, 65];
+    // let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    // let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
 
+    lifetime_syntax() 
+
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn lifetime_syntax(){
+    {
+        let x = 5;
+        let r = &x;
+        println!("r: {}", r);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("i: {:?}", i);
+    
 }
