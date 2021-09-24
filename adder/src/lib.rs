35bcf3614612f60a561711784b89b758d7c328ad
nero-15 <nero15.dev@gmail.com>
2021-09-24 21:17:45 +0900

    larger_can_hold_smaller

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,28 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
+    // #[test]
+    // fn exploration() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail");
+    // }
+    use super::*;
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
 
