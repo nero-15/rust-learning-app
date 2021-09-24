311a3878c8b17945b225d3064d0214f446e117d4
nero-15 <nero15.dev@gmail.com>
2021-09-24 21:28:39 +0900

    should_panic

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -53,6 +53,12 @@ mod tests {
             result
         );
     }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
 #[derive(Debug)]
@@ -74,4 +80,18 @@ pub fn add_two(a: i32) -> i32 {
 pub fn greeting(name: &str) -> String {
     // format!("Hello {}!", name)
     String::from("Hello!")
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
