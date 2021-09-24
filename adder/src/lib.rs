5697bccc772586eb3c63f4c0e3f8be0f3bd8026a
nero-15 <nero15.dev@gmail.com>
2021-09-24 21:16:35 +0900

    add rectangle

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,3 +10,15 @@ mod tests {
         panic!("Make this test fail");
     }
 }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
