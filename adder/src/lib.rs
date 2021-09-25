3c642ad0109fc647e4b4176054e4de965bba8111
nero-15 <nero15.dev@gmail.com>
2021-09-25 12:48:30 +0900

    Running a Subset of Tests by Name

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -71,6 +71,21 @@ mod tests {
         let value = prints_and_returns_10(8);
         assert_eq!(5, value);
     }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
 }
 
 #[derive(Debug)]
