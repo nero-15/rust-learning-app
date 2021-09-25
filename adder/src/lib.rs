a61ab1891233fd5ec2fd7c648ba9ce49138fab04
nero-15 <nero15.dev@gmail.com>
2021-09-25 12:40:41 +0900

    Showing Function Output

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -11,53 +11,65 @@ mod tests {
     // }
     use super::*;
 
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectangle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         width: 5,
+    //         height: 1,
+    //     };
 
-        assert!(larger.can_hold(&smaller));
+    //     assert!(larger.can_hold(&smaller));
+    // }
+
+    // #[test]
+    // fn smaller_cannot_hold_larger() {
+    //     let larger = Rectangle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         width: 5,
+    //         height: 1,
+    //     };
+
+    //     assert!(!smaller.can_hold(&larger));
+    // }
+
+    // #[test]
+    // fn it_adds_two() {
+    //     assert_eq!(4, add_two(2));
+    // }
+
+    // #[test]
+    // fn greeting_contains_name() {
+    //     let result = greeting("Carol");
+    //     assert!(
+    //         result.contains("Carol"),
+    //         "Greeting did not contain name, value was `{}`",
+    //         result
+    //     );
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    // fn greater_than_100() {
+    //     Guess::new(200);
+    // }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
     }
 
     #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
 
@@ -102,4 +114,9 @@ impl Guess {
 
         Guess { value }
     }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
 }
