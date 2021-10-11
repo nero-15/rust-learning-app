1e599ba8fd95d572bed0a59b526b2a1439cfefda
nero-15 <nero15.dev@gmail.com>
2021-10-11 11:27:07 +0900

    closures

--- functional_features/src/main.rs
+++ functional_features/src/main.rs
@@ -34,6 +34,14 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    let x = 4;
+
+    let equal_to_x = |z| z == x;
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
 }
 
 struct Cacher<T>
