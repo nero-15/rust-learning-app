ae7f5091dde5b62a804e8eecd4b42afc9ea01713
nero-15 <nero15.dev@gmail.com>
2021-10-16 20:58:54 +0900

    fix

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -6,7 +6,8 @@ enum List {
 use crate::List::{Cons, Nil};
 
 fn main() {
-    //boxPointers() 
+    //boxPointers()
+    deref() 
 }
 
 fn boxPointers(){
@@ -14,4 +15,12 @@ fn boxPointers(){
     // println!("b = {}", b);
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
+}
+
+fn deref(){
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
