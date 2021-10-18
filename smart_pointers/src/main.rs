2c302c9c449f1ffa349c8969ca8a24db0fc9017e
nero-15 <nero15.dev@gmail.com>
2021-10-18 21:27:56 +0900

    Creating a Reference Cycle

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -77,23 +77,41 @@ fn dropFunc(){
     println!("CustomSmartPointer dropped before the end of main.");
 }
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+// use std::rc::Rc;
+
+// fn rcFunc(){
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
-
-fn rcFunc(){
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
-
