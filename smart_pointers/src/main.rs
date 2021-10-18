a534aa6b78ac6ffb0ed9e1571eee0e36542427a4
nero-15 <nero15.dev@gmail.com>
2021-10-18 21:31:47 +0900

    fix

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -9,13 +9,14 @@ fn main() {
     //boxPointers()
     // deref() 
     // dropFunc()
-    rcFunc()
+    // rcFunc()
+    reference_cycles()
 }
 
 fn boxPointers(){
     // let b = Box::new(5);
     // println!("b = {}", b);
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
 }
 
@@ -114,4 +115,28 @@ impl List {
             Nil => None,
         }
     }
+}
+
+fn reference_cycles(){
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
