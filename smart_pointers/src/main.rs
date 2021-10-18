eccc34c99624990f8b98d3d83d90888577d9609a
nero-15 <nero15.dev@gmail.com>
2021-10-18 21:33:09 +0900

    fix

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -124,25 +124,35 @@ struct Node {
 }
 
 fn reference_cycles(){
-    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    // let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    println!("a initial rc count = {}", Rc::strong_count(&a));
-    println!("a next item = {:?}", a.tail());
+    // println!("a initial rc count = {}", Rc::strong_count(&a));
+    // println!("a next item = {:?}", a.tail());
 
-    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    // let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    println!("a rc count after b creation = {}", Rc::strong_count(&a));
-    println!("b initial rc count = {}", Rc::strong_count(&b));
-    println!("b next item = {:?}", b.tail());
+    // println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    // println!("b initial rc count = {}", Rc::strong_count(&b));
+    // println!("b next item = {:?}", b.tail());
 
-    if let Some(link) = a.tail() {
-        *link.borrow_mut() = Rc::clone(&b);
-    }
+    // if let Some(link) = a.tail() {
+    //     *link.borrow_mut() = Rc::clone(&b);
+    // }
 
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
-    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+    // println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    // println!("a rc count after changing a = {}", Rc::strong_count(&a));
 
     // Uncomment the next line to see that we have a cycle;
     // it will overflow the stack
     // println!("a next item = {:?}", a.tail());
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
 }
