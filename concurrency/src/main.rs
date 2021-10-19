cd116c87fea35537b62165d8f9f11c267ef0ddb0
nero-15 <nero15.dev@gmail.com>
2021-10-19 21:47:09 +0900

    fix

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -8,11 +8,11 @@ fn main() {
             thread::sleep(Duration::from_millis(1));
         }
     });
-
+    
+    handle.join().unwrap();
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
 
-    handle.join().unwrap();
 }
