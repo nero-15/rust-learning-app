2834ab30942c68b5a0a872baf1bea4a63b0dad0e
nero-15 <nero15.dev@gmail.com>
2021-10-20 21:39:43 +0900

    multi

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -9,14 +9,24 @@ fn main() {
 
 fn message_passing(){
     let (tx, rx) = mpsc::channel();
+    
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-        // println!("val is {}", val); error
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {}", received);
+    }
 
 }
 
