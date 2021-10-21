ba898cfa297fdb99b1017b72dda9cca89b0ae7dd
nero-15 <nero15.dev@gmail.com>
2021-10-21 22:26:28 +0900

    fix

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     // threads() 
@@ -10,14 +10,24 @@ fn main() {
 }
 
 fn shared_state(){
-    let m = Mutex::new(5);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!("m = {:?}", m);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
 
 fn message_passing(){
