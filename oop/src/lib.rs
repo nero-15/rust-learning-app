b5ac411474ab7bf745d46131b15efed2f98ff7a5
nero-15 <nero15.dev@gmail.com>
2021-10-23 22:11:14 +0900

    Screen

--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -33,3 +33,15 @@ impl AveragedCollection {
 pub trait Draw {
     fn draw(&self);
 }
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
