257358f38b0bbe7418e66771a6b482ffd0d62146
nero-15 <nero15.dev@gmail.com>
2021-10-24 15:45:56 +0900

    Ignoring Parts of a Value with a Nested _

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -85,6 +85,20 @@ fn pattern_syntax(){
             r, g, b
         ),
     }
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
