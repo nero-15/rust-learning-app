f79834fb206e0454870944e7b58e4ef0ae58c45e
nero-15 <nero15.dev@gmail.com>
2021-10-24 15:42:16 +0900

    fix

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -54,9 +54,11 @@ fn pattern_syntax(){
 
     let p = Point { x: 0, y: 7 };
 
-    let Point { x: a, y: b } = p;
-    assert_eq!(0, a);
-    assert_eq!(7, b);
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
