c2f2cbca2677eeee492b435d2858e771e42b1960
nero-15 <nero15.dev@gmail.com>
2021-10-25 21:37:01 +0900

    Creating a Safe Abstraction over Unsafe Code

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,3 +1,19 @@
+use std::slice;
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
     let mut num = 5;
 
@@ -12,6 +28,15 @@ fn main() {
     unsafe {
         dangerous();
     }
+
+    use std::slice;
+
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let slice: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+
+    
 }
 
 unsafe fn dangerous() {}
