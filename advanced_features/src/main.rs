a355609f96754fb8043b407245da7f6834442672
nero-15 <nero15.dev@gmail.com>
2021-10-27 11:35:43 +0900

    trait

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -28,9 +28,35 @@ fn add_to_count(inc: u32) {
     }
 }
 
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
     // unsafeFunc() 
-    
+    traitFunc()
+}
+
+fn traitFunc(){
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
 }
 
 fn unsafeFunc(){
