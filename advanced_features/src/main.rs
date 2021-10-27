a310455d8c39c0d0a27c5113862d90c2633b7158
nero-15 <nero15.dev@gmail.com>
2021-10-27 11:42:28 +0900

    Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -47,16 +47,47 @@ impl Add for Point {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
 fn main() {
     // unsafeFunc() 
     traitFunc()
 }
 
 fn traitFunc(){
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
-    );
+    // assert_eq!(
+    //     Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+    //     Point { x: 3, y: 3 }
+    // );
+
+    let person = Human;
+    person.fly();
 }
 
 fn unsafeFunc(){
