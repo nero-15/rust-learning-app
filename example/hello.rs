64125978a5c3b2207745ff1ca4576f9454efe2d2
nero-15 <nero15.dev@gmail.com>
2021-11-03 14:55:30 +0900

    fix

--- example/hello.rs
+++ example/hello.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!("Hello World!\nI'm a Rustacean!");
-    
-    let x = 5 + /* 90 + */ 5;
+    //println!("Hello World!\nI'm a Rustacean!");
+
+    let x = 5 +  90 +  5;
     println!("Is `x` 10 or 100? x = {}", x);
 }
