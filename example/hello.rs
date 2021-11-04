d93f89efa56ccaff2004370d1b3ec5431deb7bca
nero-15 <nero15.dev@gmail.com>
2021-11-04 22:09:23 +0900

    print

--- example/hello.rs
+++ example/hello.rs
@@ -3,4 +3,11 @@ fn main() {
 
     let x = 5 +  90 +  5;
     println!("Is `x` 10 or 100? x = {}", x);
+
+    print()
+}
+
+fn print(){
+    println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
 }
