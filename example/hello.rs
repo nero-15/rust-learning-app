691c52aa48a9d8ca8873dceb727ec6715eeff5f1
nero-15 <nero15.dev@gmail.com>
2021-11-05 21:26:52 +0900

    format

--- example/hello.rs
+++ example/hello.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Formatter, Display};
 
 fn main() {
     //println!("Hello World!\nI'm a Rustacean!");
@@ -7,6 +7,34 @@ fn main() {
     // println!("Is `x` 10 or 100? x = {}", x);
 
     print()
+}
+
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl Display for City {
+    // `f` is a buffer, and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` is like `format!`, but it will write the formatted string
+        // into a buffer (the first argument)
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
 #[derive(Debug)]
@@ -72,22 +100,39 @@ fn print(){
     // // Pretty print
     // println!("{:#?}", peter);
 
-    let minmax = MinMax(0, 14);
+    // let minmax = MinMax(0, 14);
 
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
+    // println!("Compare structures:");
+    // println!("Display: {}", minmax);
+    // println!("Debug: {:?}", minmax);
 
-    let big_range =   MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
+    // let big_range =   MinMax(-300, 300);
+    // let small_range = MinMax(-3, 3);
 
-    println!("The big range is {big} and the small is {small}",
-             small = small_range,
-             big = big_range);
+    // println!("The big range is {big} and the small is {small}",
+    //          small = small_range,
+    //          big = big_range);
 
-    let point = Point2D { x: 3.3, y: 7.2 };
+    // let point = Point2D { x: 3.3, y: 7.2 };
 
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
+    // println!("Compare points:");
+    // println!("Display: {}", point);
+    // println!("Debug: {:?}", point);
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
 }
